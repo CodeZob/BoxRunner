6e9ab4be05c1302a8321b8078fafcffdc33f0962
CodeZob <love4593@gmail.com>
2014-03-17 02:56:38 +0900

    프로젝트 시작
    1. 프로젝트 생성 및 기본 셋팅
    2. 스카이 배경 스크롤
    3. gitignore 파일 수정 ( unity + osx ) 추

---
+++ Assets/Scripts/Scroll_Mapping.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scroll_Mapping : MonoBehaviour {
+
+	public float fScrollSpeed = 0.5f;
+	float fTargetOffset;
+
+	void Update()
+	{
+		fTargetOffset += Time.deltaTime * fScrollSpeed;
+		renderer.material.mainTextureOffset = new Vector2(fTargetOffset, 0.0f);
+	}
+}
