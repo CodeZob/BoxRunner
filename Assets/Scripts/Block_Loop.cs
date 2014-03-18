b89731beca79d1a89cbffd2a2749275bf7ba474f
CodeZob <love4593@gmail.com>
2014-03-18 18:38:33 +0900

    바닥 만들기 제작중.....
    1. Cube_A 프리팹 제작
    2. Block_Loop 스크립트 제작
         - 무한 스크롤링 기능 구현

---
+++ Assets/Scripts/Block_Loop.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block_Loop : MonoBehaviour {
+
+	public float fSpeed = 3;
+	public GameObject Block;
+	public GameObject A_Zone;
+	public GameObject B_Zone;
+
+	void Update()
+	{
+		Move();
+	}
+
+	void Move()
+	{
+		A_Zone.transform.Translate(Vector3.left * fSpeed * Time.deltaTime);
+		B_Zone.transform.Translate(Vector3.left * fSpeed * Time.deltaTime);
+
+		if(B_Zone.transform.position.x <= 0)
+		{
+			SwapZone();
+			//Destroy(A_Zone);
+			//A_Zone = B_Zone;
+			//Make();
+		}
+	}
+
+	void Make()
+	{
+		B_Zone = Instantiate(Block, new Vector3(A_Zone.transform.position.x + 30, -5, 0), transform.rotation) as GameObject;
+	}
+
+	void SwapZone()
+	{
+		GameObject Temp;
+		
+		Temp = A_Zone;
+		A_Zone = B_Zone;
+		B_Zone = Temp;
+		B_Zone.transform.Translate(A_Zone.transform.position.x + 30, 0, 0);
+	}
+}
