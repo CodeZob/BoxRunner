8e1ed3e8ca3cf546d1ca51856440f2ceba01c9b3
CodeZob <love4593@gmail.com>
2014-03-19 20:28:54 +0900

    플레이어 제작
    1. 플레이어 상태머신 제작
        - 점프, 더블 점프 구현
        - 점프 후 바닥에 내려 왔을때 Run 상태로 바뀜
        - DeathZone 에 충돌시 Death 상태로 바뀜
    
    2. 코인과 충돌시 코인 삭제 구현

---
+++ Assets/Scripts/Player_Ctrl.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum PlayerState
+{
+	RUN,
+	JUMP,
+	DJUMP,
+	DEATH
+}
+
+public class Player_Ctrl : MonoBehaviour {
+
+	public PlayerState ePS;
+
+	public float fJumpPower = 500.0f;
+
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			if(ePS == PlayerState.JUMP)
+			{
+				DJump();
+			}
+
+			if(ePS == PlayerState.RUN)
+			{
+				Jump();
+			}
+		}
+	}
+
+	void OnCollisionEnter(Collision co)
+	{
+		if(ePS != PlayerState.RUN && ePS != PlayerState.DEATH)
+		{
+			Run();
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		rigidbody.WakeUp();
+		if(other.gameObject.name == "Coin")
+		{
+			Destroy(other.gameObject);
+			CoinGet();
+		}
+
+		if(other.gameObject.name == "DeathZone" && ePS != PlayerState.DEATH)
+		{
+			GameOver();
+		}
+	}
+
+	//--------------------------------------------------------------
+
+	void Jump()
+	{
+		ePS = PlayerState.JUMP;
+
+		rigidbody.AddForce(new Vector3(0.0f, fJumpPower, 0.0f));
+	}
+
+	void DJump()
+	{
+		ePS = PlayerState.DJUMP;
+
+		rigidbody.AddForce(new Vector3(0.0f, fJumpPower, 0.0f));
+	}
+
+	void Run()
+	{
+		ePS = PlayerState.RUN;
+	}
+
+	void CoinGet()
+	{
+	}
+
+	void GameOver()
+	{
+		ePS = PlayerState.DEATH;
+		this.collider.enabled = false;
+	}
+}
